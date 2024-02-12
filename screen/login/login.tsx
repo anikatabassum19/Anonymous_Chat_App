7cdca203c968a0a8b521ebcc17513d9cc658d25d
anikatabassum19 <me.blueberry19@gmail.com>
2024-02-12 20:36:40 +0600

    Initial Commit

---
+++ screen/login/login.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function login() {
+  return (
+    <View>
+      <Text className="text-6xl bg-green-100">Hello From Login</Text>
+    </View>
+  )
+}
